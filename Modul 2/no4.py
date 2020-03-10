0dd9d0a03d1a6a7b725ea51257f03d406c3e594f
naufalrandi <naufalrandialdiansyah@gmail.com>
2020-03-10 23:38:33 +0700

    Modul 2

---
+++ Modul 2/no4.py
@@ -0,0 +1,40 @@
+# nomer4
+
+
+from manusia import Manusia
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self, nama, NIM, kota, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        self.NIM = NIM
+        self.kotaTinggal = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', NIM ' + str(self.NIM) \
+            + '. Tinggal di ' + self.kotaTinggal \
+            + ' tiap bulannya.'
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, s):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+
+    def ambilKotaTinggal(self):
+        return self.kotaTinggal
+    def perbaruiKotaTinggal(self, ubah):
+        self.kotaTinggal = ubah
+    def tambahUangSaku(self, tambah):
+        self.uangSaku += tambah
+
+    listKuliah = []
+    def ambilKuliah(self, kuliah):
+        self.listKuliah.append(kuliah)
---
+++ Modul 2/no7.py
@@ -0,0 +1,17 @@
+# nomer7
+
+from no2 import Mahasiswa      
+
+
+class MhsTIF(Mahasiswa):    # class induknya : Mahasiswa
+    """Class MhsTIF yang dibangun dari class Mahasiswa"""
+    def kataKanPy(self):
+        print('Python is cool.')
+
+# Apakah metode / state itu berasal dari class Manusia, Mahasiswa, atau MhsTIF?
+
+# Jawab :
+
+# Metode atau state yang muncul berasal dari semua class baik Manusia, Mahasiswa, atau MhsTIF.
+# Ini karena MhsTIF yang merupakan anak class dari Mahasiswa, dan itu membuat MhsTIF mewarisi
+# semua properties dari Mahasiswa dan Manusia.
